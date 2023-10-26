1bee88e6fcb76a2a4082f686d8d5fa0edd865a7d
Ivan Kuzmenko <6745157+rndtrash@users.noreply.github.com>
2023-10-26 15:39:27 +0300

    Checks for [[nodiscard]]

--- c23_compat.h
+++ c23_compat.h
@@ -5,25 +5,56 @@
 #ifndef EMERGENCY_DELAY_C23_COMPAT_H
 #define EMERGENCY_DELAY_C23_COMPAT_H
 
-// https://stackoverflow.com/a/76900130
+/* nullptr
+ * https://stackoverflow.com/a/76900130
+ * https://en.cppreference.com/w/c/compiler_support/23
+ */
 
 /* C++11 or later? */
 #if (defined(__cplusplus) && __cplusplus >= 201103L)
+
 #include <cstddef>
 
 /* C2x/C23 or later? If GCC, is it version 13 or newer? */
-#elif (defined(__STDC__) && \
-        defined(__STDC_VERSION__) && \
-        (__STDC_VERSION__ >= 202000L) && \
-        (defined(__GNUC__) && __GNUC__ >= 13 || \
-        !defined(__GNUC__)))
+#elif (\
+        defined(__STDC__) && defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 202000L) || \
+        (defined(__GNUC__) && __GNUC__ >= 13 || !defined(__GNUC__))                        \
+)
 
 #include <stddef.h> /* nullptr_t */
 
 /* pre C23, pre C++11 or non-standard */
 #else
+
 #define nullptr (void*)0
-typedef void* nullptr_t;
+typedef void *nullptr_t;
+
+#endif
+
+/* nodiscard
+ * https://en.cppreference.com/w/c/compiler_support/23
+ */
+
+/* C++17 or later? C2x/C23 or later? If GCC, is it version 10 or newer? If clang, is it version 9 or newer? */
+#if (\
+        defined(__cplusplus) && __cplusplus >= 201703L || \
+        defined(__STDC__) && defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 202000L) || \
+        (defined(__GNUC__) && __GNUC__ >= 10 || !defined(__GNUC__)) || \
+        (defined(__clang_major__) && __clang_major__ >= 9)\
+)
+
+#define NODISCARD [[nodiscard]]
+
+/* Has __warn_unused_result__ ? */
+#elif __has_attribute(warn_unused_result)
+
+#define NODISCARD __attribute__((__warn_unused_result__))
+
+/* pre C23, pre C++11 or non-standard */
+#else
+
+#warning "Your compiler does not have a [[nodiscard]] support. If it actually supports this attribute, please modify the c23_compat header."
+#define NODISCARD
 
 #endif
 
